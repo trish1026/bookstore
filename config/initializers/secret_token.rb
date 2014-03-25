f588ad6e3d8dc1132a606281dca2ea6dcb6c42ff
patricia_williams <williams_patricia@bah.com>
2014-03-25 19:37:38 -0400

    initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookstore::Application.config.secret_key_base = 'de07b82f97bc8f2f06b4cd253bfaad788f798c497f6fc6d4b8e25cf159e121ffb78752a002b6989340523b656a450a1e59b41e186f8a19aeabbbd2a1e4c6d98a'
