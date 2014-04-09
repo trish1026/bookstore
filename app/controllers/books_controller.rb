f3dd1fec1740d048c6c895955b127e9bf855d3f4
“Patricia_Williamsgit config --global user.email “williams_patricia@bah.com <email_address>
2014-04-08 21:48:03 -0400

    added Lab 10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,38 +1,52 @@
 class BooksController < ApplicationController
-   def index
-   @available_at = Time.now
-   @books = Book.all
-      def show
-   @book = Book.find(params[:id])
-      end
-   end
+
+  before_action :set_book, 
+    only: [ :show, :edit, :update, :destroy ]
+
+  def index
+  @available_at = Time.now
+  @books = Book.order(:title).page(params[:page])
+  end
+
+  def show
+  end
+
    def new
      @book = Book.new
    end
+
    def create
-     @book = Book.new(book_params)
-     @book.save
-     redirect_to @book
+       @book = Book.new(book_params)
+       if @book.save
+         redirect_to @book, notice: "#{@book.title} was created!"
+       else
+         render :new
+       end
    end
+
    def edit
-     @book = Book.find(params[:id])
    end
+
    def update
-     @book = Book.find(params[:id])
-     @book.update(book_params)
-     redirect_to @book
+       if @book.update(book_params)
+         redirect_to @book
+       else
+         render :new
+       end
    end
+
    def destroy
-     @book = Book.find(params[:id])
      @book.destroy
      redirect_to books_url
    end
+
    private
+
    def book_params
      params.require(:book).permit(:title, :author, :pages, :price)
    end
-   before_action :set_book, only: [ :show, :edit, :update, :destroy ]
+
    def set_book
-  
+    @book = Book.find(params[:id])
    end
 end
